c6308ee78d45ad767a8baa1583c36d906c720c52
Grazielly Lacerda <graziilacerda19@gmail.com>
2026-09-22 19:43:39 -0300

    Refactor ActionButtons component structure and syntax

--- searchitbbs/search-it-ui-code/src/assets/components/actionButtons/action.jsx
+++ searchitbbs/search-it-ui-code/src/assets/components/actionButtons/action.jsx
@@ -1,21 +1,21 @@
-function ActionButtons() {
+export default function ActionButtons({ onGerarRecibo, onPdfSaved }) {
   return (
     <div className="actions">
 
-      <button className="clear-button">
+      <button className="clear-button" type="button">
         <i className="bi bi-trash"></i>Limpar Campos
       </button>
 
       <div>
-        <button className="generate-button">
+        <button className="generate-button" type="button" onClick={onGerarRecibo}>
           <i className="bi bi-lock"></i> Gerar Recibo
         </button>
 
-        <button className="secondary-button">
+        <button className="secondary-button" type="button">
           <i className="bi bi-printer"></i> Imprimir Recibo
         </button>
 
-        <button className="secondary-button">
+        <button className="secondary-button" type="button">
           ◉ Salvar PDF
         </button>
       </div>
@@ -23,5 +23,3 @@ function ActionButtons() {
     </div>
   );
 }
-
-export default ActionButtons;
